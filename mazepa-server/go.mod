d6b1a32e563bc2c0d07e38372a394bb90e27b442
djnn <email@djnn.sh>
2025-11-10 23:24:31 +0100

    wip(server): icmp module

--- mazepa-server/go.mod
+++ mazepa-server/go.mod
@@ -2,9 +2,13 @@ module mazepa-server
 
 go 1.24.1
 
-require github.com/spf13/cobra v1.9.1
+require (
+	github.com/spf13/cobra v1.9.1
+	golang.org/x/net v0.46.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/sys v0.37.0 // indirect
 )
--- mazepa-server/icmp/pkg.go
+++ mazepa-server/icmp/pkg.go
@@ -1,22 +1,83 @@
 package icmp
 
 import (
-	"fmt"
+	"log"
 	"net"
+	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
-/*
-   https://darkcoding.net/uncategorized/raw-sockets-in-go-ip-layer
-*/
+// The network must be "ip4:icmp" for IPv4 ICMP.
+const network = "ip4:icmp"
 
 func ListenAndPrint(addr string) {
 
-	netaddr, _ := net.ResolveIPAddr("ip4", addr)
-	conn, _ := net.ListenIP("ip4:icmp", netaddr)
+	conn, err := icmp.ListenPacket(network, "0.0.0.0")
+	if err != nil {
+		log.Fatalf("ListenPacket failed: %v\nRun with sudo or elevated permissions.", err)
+	}
+	defer conn.Close()
 
-	buf := make([]byte, 1024)
-	numRead, _, _ := conn.ReadFrom(buf)
+	log.Printf("Listening for ICMP packets on %s...", conn.LocalAddr().String())
+	rb := make([]byte, 1500)
 
-	fmt.Printf("% X\n", buf[:numRead])
+	for {
+		/* Set a read deadline to prevent the program from blocking indefinitely */
+		err := conn.SetReadDeadline(time.Now().Add(time.Second * 5))
+		if err != nil {
+			log.Printf("Error setting read deadline: %v", err)
+			continue
+		}
 
+		n, peer, err := conn.ReadFrom(rb)
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+				continue
+			}
+			log.Printf("ReadFrom failed: %v", err)
+			continue
+		}
+
+		rm, err := icmp.ParseMessage(ipv4.ICMPTypeEcho.Protocol(), rb[:n])
+		if err != nil {
+			log.Printf("ParseMessage failed: %v", err)
+			continue
+		}
+
+		switch rm.Type {
+		case ipv4.ICMPTypeEcho:
+			echoBody := rm.Body.(*icmp.Echo)
+			log.Printf("Received Echo Request from %v: ID=%d, Seq=%d",
+				peer, echoBody.ID, echoBody.Seq)
+
+			wm := icmp.Message{
+				Type: ipv4.ICMPTypeEchoReply, // Change type to Reply
+				Code: 0,
+				Body: &icmp.Echo{
+					ID:   echoBody.ID,
+					Seq:  echoBody.Seq,
+					Data: echoBody.Data,
+				},
+			}
+
+			wb, err := wm.Marshal(nil)
+			if err != nil {
+				log.Printf("Marshal failed: %v", err)
+				continue
+			}
+
+			if _, err := conn.WriteTo(wb, peer); err != nil {
+				log.Printf("WriteTo failed: %v", err)
+			}
+			log.Printf("Sent Echo Reply to %v", peer)
+
+		case ipv4.ICMPTypeEchoReply:
+			log.Printf("Received Echo Reply from %v", peer)
+
+		default:
+			log.Printf("Received unhandled ICMP message type %v from %v", rm.Type, peer)
+		}
+	}
 }
